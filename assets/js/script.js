371510c15a1f66a4a1f08d25832738aa7bf756cb
Olga <andriamiharisoaolga@yahoo.fr>
2023-06-13 07:26:59 +0200

    Mon squelette de Batman2

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const btne = document.querySelector('.demarrer');
+const sectionAjout = document.querySelector('#quizz1');
+const sectionSupprime = document.querySelector('#avant-quizz');
+
+btne.addEventListener('click', () => {
+  if (section.style.display === 'none') {
+    section.style.display = 'block';
+  } else {
+    section.style.display = 'none';
+  }
+});
+
+
+
+
+
+
+
+  
+  
+  
