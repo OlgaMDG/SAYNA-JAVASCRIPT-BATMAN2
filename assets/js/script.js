cf4c9d4d9b20f6e46c6bf124532a5eb2f99445b9
Olga <andriamiharisoaolga@yahoo.fr>
2023-06-19 22:18:24 +0200

    Finalisation du projet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,154 @@
-const btne = document.querySelector('.demarrer');
-const sectionAjout = document.querySelector('#quizz1');
-const sectionSupprime = document.querySelector('#avant-quizz');
+window.onload=()=>{
 
-btne.addEventListener('click', () => {
-  if (section.style.display === 'none') {
-    section.style.display = 'block';
-  } else {
-    section.style.display = 'none';
+    //Pop Up
+    //let myForm =document.getElementById('formulaire');//les deux commentaires travaillent ensemble, soit tu mets listener en submit et getId de formulaire, soit listener submit et tu vise getId du button
+    let myForm =document.getElementById('validation');
+    //myForm.addEventListener('submit', (e)=>{
+    myForm.addEventListener('click', (e)=>{
+      
+      let email= document.querySelector('.champ-email');//comme email=document.getElementById('IdInput');
+      let mySelect = document.querySelector('.mySelect');
+      let myTextarea = document.getElementById("myTextarea");
+      
+      if(email.value==='' || mySelect.value==='' || myTextarea.value===''){
+        e.preventDefault();
+
+        let invalidMail=document.getElementById('inavlid-mail');
+        invalidMail.innerHTML="S'il vous plaît, entrez un mail";//Message d'erreur comme
+        invalidMail.style.color="red";
+        invalidMail.style.fontStyle="italic";
+        
+        let invalidSelect = document.getElementById('invalid-select');
+        invalidSelect.innerHTML ="S'il vous plaît, choisissez une fréquence pour votre newsletter";
+        invalidSelect.style.color="red";
+        invalidSelect.style.fontStyle="italic";
+
+        let invalidTextarea =  document.getElementById('invalidTextarea');
+        invalidTextarea.innerHTML="S'il vous plaît, entrez votre message";
+        invalidTextarea.style.color="red";
+        invalidTextarea.style.fontStyle="italic";
+
+          email.addEventListener('input', function() {
+            if (email.value.trim() !== '') {//Enlever le message d'erreur pour le mail
+              invalidMail.innerHTML = '';
+            }
+          });
+
+
+          mySelect.addEventListener('input', function() {
+            if (mySelect.value.trim() !== '') {
+              invalidSelect.innerHTML = '';
+            }
+          });
+
+          myTextarea.addEventListener('input', function() {
+            if (myTextarea.value.trim() !== '') {
+              invalidTextarea.innerHTML = '';
+            }
+          });
+
+      }
+      else{
+        e.preventDefault();
+        let popupMontre= document.getElementById('popup');
+        popupMontre.style.display='block';
+        window.scrollTo({
+          top: document.getElementById('contact').offsetTop
+        });
+      }
+  })
+  let popupMontre= document.getElementById('popup');
+
+  document.addEventListener('click', (e) => {
+    if (popupMontre.contains(e.target)) {
+      
+      // Masquer le popup en cliquant dans le popup
+      popupMontre.style.display = 'none';
+      let email= document.querySelector('.champ-email');
+      let mySelect = document.querySelector('.mySelect');
+      let myTextarea = document.getElementById("myTextarea");
+      email.value = '';
+      mySelect.value = '';
+      myTextarea.value = '';
+    }
+  });
+
+  //Scrolling progressif sur les boutons Batman
+  $(document).ready(function() {
+    // Défilement en douceur lorsqu'un lien est cliqué
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $($(this).attr('href'));
+
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 800);
+        }
+    });
+  });
+
+  //Apparition progressive des éléments au fur et à mesure du scrolling, avec une classe reveal
+  const ratio = .1
+  const options = {
+    root: null,
+    rootMargin: '0px', //marge d'apparition
+    threshold: ratio //10% l'objet qui apparaît et on va déjà avoir notre animation
   }
-});
 
+  const handleIntersect = function (entries, observer) {
+    entries.forEach(function (entry){
+      if(entry.intersectionRatio > ratio){
+        entry.target.classList.add('reveal-visible')
+        observer.unobserve(entry.target)
+      }
+    })
+  }
 
+  const observer = new IntersectionObserver(handleIntersect, options)
+ document.querySelectorAll('.reveal').forEach(function (r){
+    observer.observe(r)
+  })
 
+  //Zoom image en scrollant
+  $(window).scroll(function() {
+    var scrollTop = $(this).scrollTop();
+    var windowHeight = $(this).height();
+    
+    $('.zoom-image').each(function() {
+      var imagePosition = $(this).offset().top;
+      
+      if (imagePosition < scrollTop + windowHeight) {
+        var scrollPercentage = (scrollTop + windowHeight - imagePosition) / windowHeight;
+        var zoomLevel = scrollPercentage * 100;
+        
+        $(this).css({
+          'opacity': 1,
+          'transform': 'scale(' + zoomLevel + '%)'
+        });
+      } else {
+        $(this).css({
+          'opacity': 0,
+          'transform': 'scale(0)'
+        });
+      }
+    });
+  });
 
-
-
-
+  //Notre souris
+  var container = document.querySelector('.container-souri');
+  var bat = document.querySelector('#bat');
   
+  container.addEventListener('mousemove', function(e) {
+    var x = e.clientX;
+    var y = e.clientY;
   
+    bat.style.opacity = '1';
+    bat.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+  });
   
+  container.addEventListener('mouseout', function() {
+    bat.style.opacity = '0';
+  });
+
+}
---
+++ assets/js/script2.js
@@ -0,0 +1,179 @@
+  $(document).ready(function() {
+    var questions = [];
+    var currentQuestion = 0;
+    var currentQuestionIndex = 0;
+    var score = 0;
+
+  //les images
+    var questionImages = {
+        "Quel est l’autre nom de l’Homme-Mystère ?": "./assets/Illustrations/Batgame_3.png",
+        "Quelle est l’ancienne profession de Harley Quinn ?": "./assets/Illustrations/Batgame_4.png",
+        "Quel est l’objet fétiche de Double Face ?": "./assets/Illustrations/Batgame_5.png",
+        "Quelle ville Batman défend-il ?": "./assets/Illustrations/Batgame_10.png",
+        "Tim Burtin a réalisé deux Batman, qui jouait Batman ?": "./assets/Illustrations/Batgame_11.png",
+        "Quel est le prénom des parents du jeune Bruce Wayne ?": "./assets/Illustrations/Batgame_18.png",
+        "Dans son premier Batman (1989) Jack Nicholson jouait :": "./assets/Illustrations/Batgame_12.png",
+        " Qui interprète le Joker en 2008 ?": "./assets/Illustrations/Batgame_19.png",
+        "En quelle année Robin fait il sa première apparition ?": "./assets/Illustrations/Batgame_20.png",
+        "Qui est la fille de Batman et Catwoman (Earth - 2) ?": "./assets/Illustrations/Batgame_21.png",
+        "Batman c’est aussi le nom d’une ville en...": "./assets/Illustrations/Batgame_7.png",
+        "Qui a realisé Batman en 1966 ?": "./assets/Illustrations/Batgame_6.png"
+      };
+    // Fonction pour récupérer les questions via AJAX
+    function fetchQuestions() {
+      $.ajax({
+        url: "https://batman-api.sayna.space/questions",
+        dataType: 'json',
+        success: function(data) {
+          questions = data;
+          showQuestion(0);
+          $("#avant-quizz").hide();
+          $("#question-container").show();
+        },
+        error: function() {
+          alert("Une erreur s'est produite lors de la récupération des questions.");
+        }
+      });
+      
+    }
+  
+    // Fonction pour afficher une question
+    function showQuestion(index) {
+        var question = questions[index];
+        $("#question").text(question.question);
+      
+        var image = questionImages[question.question];
+        if (image) {
+            $("#image-container").attr("src", image); // Mettre à jour l'attribut src avec l'image correspondante
+            $("#image-container").show(); // Afficher l'image
+        } else {
+            $("#image-container").hide(); // Masquer l'image si elle n'est pas définie
+        }
+        currentQuestion = index + 1; // Mettre à jour le numéro de la question actuelle
+
+      // Réinitialiser la sélection de réponse
+      $("input[name='response']").prop("checked", false);
+
+      var question = questions[index];
+      $("#question-title").text(currentQuestion + "/" + questions.length);
+      $("#question").text(question.question);
+      $("#response0").text(question.response[0].text);
+      $("#response1").text(question.response[1].text);
+      $("#response2").text(question.response[2].text);
+    }
+  
+// Fonction pour afficher le score final
+function showScore() {
+  var message = "Votre score : " + score + " / " + questions.length;
+  $("#answers-container").text(message);
+  $("#next-button").hide();
+  $("#show-answers-button").hide();
+  
+  // Cacher le quizz
+  $("#question-container").hide();
+  
+  // Afficher le message de score
+  $("#score-message").text("Votre score est de " + score + " sur " + questions.length + ".");
+  $("#score-message").show();
+}
+
+//Fonction pour vérifier la réponse sélectionnée
+function checkAnswer(index) {
+    var question = questions[index];
+    var selectedResponse = $("input[name='response']:checked").val();
+  
+    if (selectedResponse !== undefined) {
+      if (question.response[selectedResponse].isGood) {
+        score++;
+      }
+  
+      if (index < questions.length - 1) {
+        showQuestion(index + 1);
+        currentQuestionIndex = index + 1; // Mise à jour de l'index de la question actuelle
+      } else {
+        showScore();
+      }
+    } else {
+      //alert("Veuillez sélectionner une réponse.");
+      var title = "Erreur";
+      var content = "Veuillez sélectionner une réponse.";
+    
+      $("#dialog-title").text(title);
+      $("#dialog-content").text(content);
+      $("#dialog-box").show();
+    }
+  }
+
+function showScore() {
+  var titreMessageScore = "";
+  var message = "";
+
+  if (score >= 0 && score <= 4) {
+    titreMessageScore = score + "/" + questions.length + " C'est pas tout à fait ça...";
+    message = "Oula! Heuresement que le Riddler est sous les verrous... Il faut que vous vous repassiez les films, cette fois en enlevant peut-être le masque qui vous a bloqué la vue! Alors, rien n'est perdu!";
+  } else if (score >= 5 && score <= 9) {
+    titreMessageScore =  score + "/" + questions.length + " Pas mal!";
+    message = "Encore un peu d'entrainement avec le chevalier Noir, vous serait bénéfique, mais vous pouvez marcher la tête haute, vos connaissances sont là. A vous de les consolider, foncez, Gotham est votre terrain de chasse!";
+  } else if (score >= 10 && score <= 12) {
+    titreMessageScore = score + "/" + questions.length +  " Bravo!";
+    message = "Vous êtes véritablement un super fan de l'univers de Batman! Comics, films, rien ne vous échappe. Bruce Wayne a de quoi être fier, Gotham est en paix et Batman peut prendre sa retraite, vous veillez aux grains!";
+  } else {
+    titreMessageScore = "Score inconnu";
+    message = "Score non valide";
+  }
+
+  //titreMessageScore += " (" + score + "/" + questions.length + ")";
+  
+  $("#titre-message").text(titreMessageScore);
+  $("#score-message").text(message);
+  
+  // Masquer les éléments du quizz
+  $("#question-container").hide();
+  $("#next-button").hide();
+  
+  // Afficher le pop-up du score
+  $("#score-popup").show();
+}
+
+// Gestionnaire d'événement pour le bouton "Recommencer le quizz"
+$("#restart-button").click(function() {
+  // Cacher le pop-up du score
+  $("#score-popup").hide();
+
+  // Réinitialiser les variables du quizz
+  currentQuestion = 0;
+  currentQuestionIndex = 0;
+  score = 0;
+
+  // Afficher à nouveau le quizz
+  $("#question-container").show();
+  $("#next-button").show();
+  $("#show-answers-button").show();
+
+  // Charger les questions
+  fetchQuestions();
+});
+
+
+// Gestionnaire d'événement pour le bouton "Démarrer"
+$("#start-button").click(function() {
+  fetchQuestions();
+});
+
+// Gestionnaire d'événement pour le bouton "Question suivante"
+$("#next-button").click(function() {
+  checkAnswer(currentQuestionIndex);
+});
+
+// Gestionnaire d'événement pour le bouton "Afficher les réponses"
+$("#show-answers-button").click(function() {
+  showScore();
+});
+
+$("#dialog-button").click(function() {
+  // Cacher la boîte de dialogue
+  $("#dialog-box").hide();
+});
+
+
+});
